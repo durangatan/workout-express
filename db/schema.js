42ad99daf5b3fd14ec22bc9246eaea582788e21f
duranje <joeduran8@gmail.com>
2019-02-19 11:47:02 -0600

    fixes db:create script

--- db/schema.js
+++ db/schema.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 import { DatabaseConnector, dbConfig } from './';
 
 const createTableStatements = {
-  exercises: `CREATE TABLE IF NOT EXISTS exercises (
+	exercises: `CREATE TABLE IF NOT EXISTS exercises (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		machineId VARCHAR(255),
 		name VARCHAR(255) UNIQUE,
@@ -11,31 +11,25 @@ const createTableStatements = {
 		notes VARCHAR(255),
 		primary key (id)
 	);`,
-  routines: `CREATE TABLE IF NOT EXISTS routines (
+	routines: `CREATE TABLE IF NOT EXISTS routines (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		name VARCHAR(255) UNIQUE,
 		primary key (id)
 	);`,
-  workouts: `CREATE TABLE IF NOT EXISTS workouts (
+	workouts: `CREATE TABLE IF NOT EXISTS workouts (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		startTime Int unsigned,
 		endTime INT unsigned,
 		primary key (id)
 	);`,
-  workoutRoutines: `CREATE TABLE IF NOT EXISTS workout_routines (
+	workoutRoutines: `CREATE TABLE IF NOT EXISTS workout_routines (
 		id INT NOT NULL AUTO_INCREMENT,
 		workoutId INT unsigned NOT NULL,
 		routineId INT unsigned NOT NULL,
 		CONSTRAINT workout_routine_unique UNIQUE (workoutId, routineId),
-		primary key (id),
+		primary key (id)
 	);`,
-  completedSets: `CREATE TABLE IF NOT EXISTS completed_sets (
-  	id INT unsigned NOT NULL AUTO_INCREMENT,
-  	routineSetId INT unsigned NOT NULL,
-  	workoutId INT unsigned NOT NULL,
-  	primary key (id)
-  );`,
-  workoutSets: `CREATE TABLE IF NOT EXISTS workout_sets (
+	workoutSets: `CREATE TABLE IF NOT EXISTS workout_sets (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		weight INT unsigned NOT NULL,
 		repetitions INT unsigned NOT NULL,
@@ -44,14 +38,14 @@ const createTableStatements = {
 		exerciseId INT unsigned NOT NULL,
 		primary key (id)
 	);`,
-  routineSets: `CREATE TABLE IF NOT EXISTS routine_sets (
+	routineSets: `CREATE TABLE IF NOT EXISTS routine_sets (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		routineId INT unsigned NOT NULL,
 		setId INT unsigned NOT NULL,
 		ordering INT unsigned NOT NULL,
 		primary key (id)
 	);`,
-  completedSets: `CREATE TABLE IF NOT EXISTS completed_sets (
+	completedSets: `CREATE TABLE IF NOT EXISTS completed_sets (
 		id INT unsigned NOT NULL AUTO_INCREMENT,
 		workoutId INT unsigned NOT NULL,
 		setId INT unsigned NOT NULL,
@@ -60,12 +54,18 @@ const createTableStatements = {
 	);`
 };
 
-const db = new DatabaseConnector(dbConfig);
+const dbConfigWithoutDatabase = { ...dbConfig, database: null };
 
-db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME};`)
-  .then(db.query(`USE ${DB_NAME}`))
-  .then(() =>
-    Promise.all(Object.values(createTableStatements).map(createTableStatement => db.query(createTableStatement)))
-  )
-  .then(() => db.close())
-  .then(() => process.exit());
+const db = new DatabaseConnector(dbConfigWithoutDatabase);
+
+db.query(`CREATE DATABASE IF NOT EXISTS ebc_workout_dev;`)
+	.then(db.query(`USE ebc_workout_dev;`))
+	.then(() =>
+		Promise.all(
+			Object.values(createTableStatements).map(createTableStatement =>
+				db.query(createTableStatement)
+			)
+		)
+	)
+	.then(() => db.close())
+	.then(() => process.exit());
