06dcc6dd2f5ff500f4b986c228d576dcddc77d22
duranje <joeduran8@gmail.com>
2019-03-04 15:20:44 -0600

    heroku sicks duck

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import express from 'express';
-import { RoutineController, WorkoutController, ExerciseController } from './controllers';
+import {
+	RoutineController,
+	WorkoutController,
+	ExerciseController
+} from './controllers';
 
 const app = express();
-app.listen(3001, () => console.log('Server listening at port 3001.'));
+app.listen(process.env.PORT || 3001, () =>
+	console.log(`Server listening at port ${process.env.PORT || 3000}.`)
+);
 
 // enable cors for now
 app.use(function(_, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
 });
 
 // logging middleware
 app.use(function(req, res, next) {
-  console.log(`ebc-workout-express:${req.method.toUpperCase()}:${req.originalUrl}`);
-  next();
+	console.log(
+		`ebc-workout-express:${req.method.toUpperCase()}:${req.originalUrl}`
+	);
+	next();
 });
 
 app.use('/routines', RoutineController);
@@ -22,5 +33,5 @@ app.use('/workouts', WorkoutController);
 app.use('/exercises', ExerciseController);
 
 app.get('/', function(req, res) {
-  res.send('boo');
+	res.send('boo');
 });
